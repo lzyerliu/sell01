ffc6860ac7e317af333a47a09e57f91056edbe81
lzyerliu <lzy11121112@sina.com>
2018-04-10 22:25:16 +0800

    提交文件

---
+++ src/components/lineTitle/lineTitle.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="line-title">
+		<div class="line" :style="lineColor"></div>
+		<div class="text">{{textTitle}}</div>
+		<div class="line" :style="lineColor"></div>
+	</div>
+</template>
+
+<script type="ecmascript-6">
+	export default {
+		props: {
+			textTitle: {
+				type: String
+			},
+			lineColor: {
+				type: String
+			}
+		}
+	};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.line-title
+		display: flex
+		.line
+			flex: 1
+			position: relative
+			top: -6px
+		.text
+			padding: 0 12px
+			font-size: 14px
+			font-weight: 700
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
+
+import './common/stylus/common.styl';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+let routes = [
+  { path: '/', redirect: 'goods' },
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
+];
+let router = new VueRouter({
+	linkActiveClass: 'active',
+	routes
+});
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app');
